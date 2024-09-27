3f16e9eda8f60ad2bad652492959aea87c560824
Cesar Martinez <cmmh200427@gmail.com>
2024-09-27 02:02:36 -0400

    agregados algunos verifytoken

--- src/routes/patient.routes.ts
+++ src/routes/patient.routes.ts
@@ -1,28 +1,34 @@
-import express from 'express';
+import express from "express";
 import { PatientController } from "../controllers";
-import { PatientValidator } from '../validators';
-import { validateFields } from '../middlewares/validateFields.middleware';
-import upload from '../middlewares/handleHistory.middleware.';
-
+import { PatientValidator } from "../validators";
+import { validateFields } from "../middlewares/validateFields.middleware";
+import upload from "../middlewares/handleHistory.middleware.";
+import { verifyToken } from "../middlewares/verifyTokem.middleware";
 
 const router = express.Router();
 
 const patientValidator = new PatientValidator();
 const patientController = new PatientController();
 
-router.get('/getAllActive', patientController.getAllActive);
-router.get('/getAll', patientController.getAll);
-router.get('/getById/:id', patientController.getById);
-router.post('/create', 
-    validateFields, 
-    patientValidator.createValidate, 
-    upload.array('images', 5), 
-    patientController.create);
+router.get("/getAllActive", verifyToken, patientController.getAllActive);
+router.get("/getAll", verifyToken, patientController.getAll);
+router.get("/getById/:id", verifyToken, patientController.getById);
+router.post(
+  "/create",
+  validateFields,
+  patientValidator.createValidate,
+  upload.array("images", 5),
+  verifyToken,
+  patientController.create
+);
 
-router.put('/update/:id', validateFields, patientValidator.createValidate,patientController.update);
-router.delete('/delete/:id',patientController.deletePatient);
-
-
-
+router.put(
+  "/update/:id",
+  validateFields,
+  patientValidator.createValidate,
+  verifyToken,
+  patientController.update
+);
+router.delete("/delete/:id", verifyToken, patientController.deletePatient);
 
 module.exports = router;
