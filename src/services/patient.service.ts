00c47d75291b469fa2494a4e103555f5081d3a76
Cesar Martinez <cmmh200427@gmail.com>
2024-09-27 05:34:28 -0400

    login configurado para iniciar sesion con correo

--- src/services/patient.service.ts
+++ src/services/patient.service.ts
@@ -1,25 +1,36 @@
 import e from "express";
-import { CommunityDB, DeliveryDB, MedicalHistoryDB, MedicationDB, PathologyDB, PathologyPatientDB, PatientDB, ReturnDB, sequelize, TreatmentDB } from "../config/sequelize.conf";
-import { PatientInterface, } from "../interfaces";
+import {
+  CommunityDB,
+  DeliveryDB,
+  MedicalHistoryDB,
+  MedicationDB,
+  PathologyDB,
+  PathologyPatientDB,
+  PatientDB,
+  ReturnDB,
+  sequelize,
+  TreatmentDB,
+} from "../config/sequelize.conf";
+import { PatientInterface } from "../interfaces";
 
 export const getAll = async () => {
   try {
-    const Patient = await  PatientDB.findAll({
-      attributes: { exclude: ['id', 'community_id', 'updatedAt'] },
+    const Patient = await PatientDB.findAll({
+      attributes: { exclude: ["community_id", "updatedAt"] },
       include: [
         {
           model: CommunityDB,
-          as: 'community',
-          attributes: ['name']
+          as: "community",
+          attributes: ["name"],
         },
         {
           model: PathologyDB,
-          as: 'pathologies',
-          attributes: ['name'],
+          as: "pathologies",
+          attributes: ["name"],
           through: {
-            attributes: ['description']
-          }
-        }
+            attributes: ["description"],
+          },
+        },
       ],
     });
     return {
@@ -37,29 +48,29 @@ export const getAll = async () => {
   }
 };
 
-export const getById = async (id:number) => {
+export const getById = async (id: number) => {
   try {
     const Patient = await PatientDB.findOne({
       where: { id_card: id },
-      attributes: { exclude: ['id', 'community_id', 'updatedAt'] },
+      attributes: { exclude: ["id", "community_id", "updatedAt"] },
       include: [
         {
           model: CommunityDB,
-          as: 'community',
-          attributes: ['name']
+          as: "community",
+          attributes: ["name"],
         },
         {
           model: PathologyDB,
-          as: 'pathologies',
-          attributes: ['name'],
+          as: "pathologies",
+          attributes: ["name"],
           through: {
-            attributes: ['description']
-          }
-        }
+            attributes: ["description"],
+          },
+        },
       ],
     });
-  
-    if(!Patient){
+
+    if (!Patient) {
       return {
         message: `Patient with id ${id} not found`,
         status: 404,
@@ -73,7 +84,7 @@ export const getById = async (id:number) => {
       },
     };
   } catch (error) {
-    console.log(error)
+    console.log(error);
     return {
       message: `Contact the administrator: error`,
       status: 500,
@@ -81,13 +92,13 @@ export const getById = async (id:number) => {
   }
 };
 
-export const getFullPatient = async (id:number) => {
+export const getFullPatient = async (id: number) => {
   try {
     const Patient = await PatientDB.findOne({
       where: { id_card: id },
     });
-  
-    if(!Patient){
+
+    if (!Patient) {
       return {
         message: `Patient with id ${id} not found`,
         status: 404,
@@ -96,30 +107,30 @@ export const getFullPatient = async (id:number) => {
 
     const patientFicha = await PatientDB.findOne({
       where: { id: Patient!.id },
-      attributes: { exclude: ['community_id', 'updatedAt'] },
+      attributes: { exclude: ["community_id", "updatedAt"] },
       include: [
         //patologías del paciente
         {
           model: PathologyDB,
           through: { attributes: [] },
-          attributes: ['name'],  
+          attributes: ["name"],
         },
         //tratamientos del paciente
         {
           model: TreatmentDB,
-          attributes: { exclude: ['patient_id', 'updatedAt'] },
+          attributes: { exclude: ["patient_id", "updatedAt"] },
           include: [
             {
               model: MedicationDB,
               attributes: {
-                exclude: ["quantity",'createdAt', 'updatedAt']
+                exclude: ["quantity", "createdAt", "updatedAt"],
               },
-              through: { 
+              through: {
                 attributes: ["quantity"],
-                as: "medication_quantity"
+                as: "medication_quantity",
               },
-            }
-          ]
+            },
+          ],
         },
         //medicamentos del paciente
         {
@@ -127,8 +138,8 @@ export const getFullPatient = async (id:number) => {
           include: [
             {
               model: MedicationDB,
-              through: { attributes: ['quantity'] },  
-              attributes: ['name'],
+              through: { attributes: ["quantity"] },
+              attributes: ["name"],
             },
             {
               model: ReturnDB,
@@ -136,17 +147,17 @@ export const getFullPatient = async (id:number) => {
                 {
                   model: MedicationDB,
                   attributes: {
-                    exclude: ["quantity",'createdAt', 'updatedAt']
+                    exclude: ["quantity", "createdAt", "updatedAt"],
                   },
-                  through: { attributes: ['quantity'] },  
-                }
-              ]
-            }
-          ]
+                  through: { attributes: ["quantity"] },
+                },
+              ],
+            },
+          ],
         },
-      ]
+      ],
     });
-    
+
     return {
       message: `Successful Patient connection`,
       status: 200,
@@ -155,7 +166,7 @@ export const getFullPatient = async (id:number) => {
       },
     };
   } catch (error) {
-    console.log(error)
+    console.log(error);
     return {
       message: `Contact the administrator: error`,
       status: 500,
@@ -163,73 +174,74 @@ export const getFullPatient = async (id:number) => {
   }
 };
 
-
-
 export const getAllActive = async () => {
-    try {
-        const Patient = await  PatientDB.findAll({
-        where:{status:"active"},
-        attributes: { exclude: ['id', 'community_id', 'updatedAt'] },
-        include: [
-          {
-            model: CommunityDB,
-            as: 'community',
-            attributes: ['name']
-          },
-          {
-            model: PathologyDB,
-            as: 'pathologies',
-            attributes: ['name'],
-            through: {
-              attributes: ['description']
-            }
-          }
-        ],
-      });
-      return {
-        message: `Successful Patient connection`,
-        status: 200,
-        data: {
-            Patient: Patient,
+  try {
+    const Patient = await PatientDB.findAll({
+      where: { status: "active" },
+      attributes: { exclude: ["community_id", "updatedAt"] },
+      include: [
+        {
+          model: CommunityDB,
+          as: "community",
+          attributes: ["name"],
         },
-      };
-    } catch (error) {
-      return {
-        message: `Contact the administrator: error`,
-        status: 500,
-      };
-    }
+        {
+          model: PathologyDB,
+          as: "pathologies",
+          attributes: ["name"],
+          through: {
+            attributes: ["description"],
+          },
+        },
+      ],
+    });
+    return {
+      message: `Successful Patient connection`,
+      status: 200,
+      data: {
+        Patient: Patient,
+      },
+    };
+  } catch (error) {
+    return {
+      message: `Contact the administrator: error`,
+      status: 500,
+    };
+  }
 };
 
 export const create = async (data: PatientInterface) => {
   const t = await sequelize.transaction();
   try {
     const patientCedula = await PatientDB.findOne({
-      where: { id_card: data.id_card }
+      where: { id_card: data.id_card },
     });
 
     if (patientCedula) {
       return {
         message: `patient with cedula ${data.id_card} already exists`,
         status: 400,
-        data: {}
+        data: {},
       };
     }
 
     const { pathologies, images } = data;
-    console.log(pathologies) 
-    console.log(images) 
+    console.log(pathologies);
+    console.log(images);
 
-    const Patient = await PatientDB.create({
-      ...data,
-    }, { transaction: t });
+    const Patient = await PatientDB.create(
+      {
+        ...data,
+      },
+      { transaction: t }
+    );
 
     const pathologiesArray = pathologies!.map((pathology) => {
       return {
         patient_id: Patient.id,
         pathology_id: pathology.id_pathology,
         description: pathology.description,
-      }
+      };
     });
 
     await PathologyPatientDB.bulkCreate(pathologiesArray, { transaction: t });
@@ -238,34 +250,34 @@ export const create = async (data: PatientInterface) => {
       return {
         patient_id: Patient.id,
         url: image,
-      }
+      };
     });
 
     await MedicalHistoryDB.bulkCreate(imagesArray, { transaction: t });
 
     const patient = await PatientDB.findOne({
       where: { id: Patient.id },
-      attributes: { exclude: ['id', 'community_id', 'updatedAt'] },
+      attributes: { exclude: ["id", "community_id", "updatedAt"] },
       include: [
         {
           model: CommunityDB,
-          as: 'community',
-          attributes: ['name']
+          as: "community",
+          attributes: ["name"],
         },
         {
           model: PathologyDB,
-          as: 'pathologies',
-          attributes: ['name'],
+          as: "pathologies",
+          attributes: ["name"],
           through: {
-            attributes: ['description']
-          }
+            attributes: ["description"],
+          },
         },
         {
           model: MedicalHistoryDB,
-          attributes: ['url']
-        }
+          attributes: ["url"],
+        },
       ],
-      transaction: t
+      transaction: t,
     });
 
     await t.commit();
@@ -277,7 +289,6 @@ export const create = async (data: PatientInterface) => {
         Patient: patient,
       },
     };
-
   } catch (error) {
     await t.rollback();
     console.log(error);
@@ -288,63 +299,65 @@ export const create = async (data: PatientInterface) => {
   }
 };
 
-
 //TODO update patient
-export const update = async (id:number, data:PatientInterface) => {
+export const update = async (id: number, data: PatientInterface) => {
   const t = await sequelize.transaction();
-  try { 
-    const patientCedula = await  PatientDB.findOne({
-      where:{id}
+  try {
+    const patientCedula = await PatientDB.findOne({
+      where: { id },
     });
-      
+
     if (!patientCedula) {
       return {
         message: `patient with id:${data.id_card} doenst exists`,
-        status: 400, 
-        data:{}
+        status: 400,
+        data: {},
       };
     }
 
-    const {pathologies} = data;
+    const { pathologies } = data;
 
-    await  PatientDB.update({
-      ...data,
-      }, {where:{id}, transaction: t});
+    await PatientDB.update(
+      {
+        ...data,
+      },
+      { where: { id }, transaction: t }
+    );
 
     const pathologiesArray = pathologies!.map((pathology) => {
       return {
         patient_id: id,
         pathology_id: pathology.id_pathology,
         description: pathology.description,
-      }
+      };
     });
 
     //delete all pathologies of the patient
     await PathologyPatientDB.destroy({
-      where: {patient_id: id},
-      transaction: t
+      where: { patient_id: id },
+      transaction: t,
     });
 
-    await PathologyPatientDB.bulkCreate(pathologiesArray, {transaction: t});
+    await PathologyPatientDB.bulkCreate(pathologiesArray, { transaction: t });
     const patient = await PatientDB.findOne({
-      where: {id},
-      attributes: { exclude: ['id', 'community_id', 'updatedAt'] },
+      where: { id },
+      attributes: { exclude: ["id", "community_id", "updatedAt"] },
       include: [
         {
           model: CommunityDB,
-          as: 'community',
-          attributes: ['name']
+          as: "community",
+          attributes: ["name"],
         },
         {
           model: PathologyDB,
-          as: 'pathologies',
-          attributes: ['name'],
+          as: "pathologies",
+          attributes: ["name"],
           through: {
-            attributes: ['description']
-          }
-        }
+            attributes: ["description"],
+          },
+        },
       ],
-      transaction: t
+      transaction: t,
     });
 
     await t.commit();
@@ -363,15 +376,22 @@ export const update = async (id:number, data:PatientInterface) => {
       status: 500,
     };
   }
-}
+};
 
 export const getPriorityPatients = async () => {
   try {
     const vulnerablePatients = await PatientDB.findAll({
       where: {
-        status: 'active',  // Solo pacientes activos
+        status: "active", // Solo pacientes activos
       },
-      attributes: ['first_name', 'last_name', 'economic_status', 'vulnerability_level', 'phone', 'address'],
+      attributes: [
+        "first_name",
+        "last_name",
+        "economic_status",
+        "vulnerability_level",
+        "phone",
+        "address",
+      ],
       order: [
         sequelize.literal(`
           CASE 
@@ -392,10 +412,10 @@ export const getPriorityPatients = async () => {
             WHEN economic_status = 'clase alta' THEN 5
             ELSE 6
           END
-        `)
-      ]
+        `),
+      ],
     });
-    
+
     return {
       message: `Successful Patient connection`,
       status: 200,
@@ -411,22 +431,25 @@ export const getPriorityPatients = async () => {
   }
 };
 
-export const getRangePatients = async (data:any) => {
+export const getRangePatients = async (data: any) => {
   try {
-
-    const {vulnerabilityLevel, economicStatus} = data;
-
-    
-
+    const { vulnerabilityLevel, economicStatus } = data;
 
     const filteredPatients = await PatientDB.findAll({
-    where: {
-      status: 'active',  // Solo pacientes activos
-      ...data  // Aplicamos los filtros dinámicos
-    },
-      attributes: ['first_name', 'last_name', 'economic_status', 'vulnerability_level', 'phone', 'address'],
+      where: {
+        status: "active", // Solo pacientes activos
+        ...data, // Aplicamos los filtros dinámicos
+      },
+      attributes: [
+        "first_name",
+        "last_name",
+        "economic_status",
+        "vulnerability_level",
+        "phone",
+        "address",
+      ],
     });
-    
+
     return {
       message: `Successful Patient connection`,
       status: 200,
@@ -442,24 +465,21 @@ export const getRangePatients = async (data:any) => {
   }
 };
 
-export const deletePatient = async (id:number) => {
-    try {
-      const Patient = await  PatientDB.update(
-        {status:"deleted"},
-        {where:{id}}
-      );
+export const deletePatient = async (id: number) => {
+  try {
+    const Patient = await PatientDB.update(
+      { status: "deleted" },
+      { where: { id } }
+    );
 
-      return {
-        message: `Patient with id ${id} Successfully deleted`,
-        status: 200,
-      };
-    } catch (error) {
-      return {
-        message: `Contact the administrator: error`,
-        status: 500,
-      };
-    }
-  };
-
-
-
+    return {
+      message: `Patient with id ${id} Successfully deleted`,
+      status: 200,
+    };
+  } catch (error) {
+    return {
+      message: `Contact the administrator: error`,
+      status: 500,
+    };
+  }
+};
